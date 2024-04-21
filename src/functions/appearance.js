5a14be75538aecf28cdac9e4e9fb664b86d5d909
ValenciaArcega <valencia.arcega@gmail.com>
2024-04-21 11:29:38 -0600

    feature: new experience and education card with dark mode fixes

---
+++ src/functions/appearance.js
@@ -0,0 +1,34 @@
+let root = document.documentElement
+const btnMobileDark = document.querySelector(".topBar-btnDark")
+const btnMobileLight = document.querySelector(".topBar-btnLight")
+
+const btnDesktopDark = document.querySelector(".navBar-desktop-btnDark")
+const btnDesktopLight = document.querySelector(
+	".navBar-desktop-btnLight"
+)
+
+function makeDark() {
+	root.id = "rootDarkMode"
+
+	btnMobileDark.classList.add("hidden")
+	btnMobileLight.classList.remove("hidden")
+
+	btnDesktopDark.classList.add("hidden")
+	btnDesktopLight.classList.remove("hidden")
+}
+
+function makeLight() {
+	root.removeAttribute("id")
+
+	btnMobileLight.classList.add("hidden")
+	btnMobileDark.classList.remove("hidden")
+
+	btnDesktopLight.classList.add("hidden")
+	btnDesktopDark.classList.remove("hidden")
+}
+
+btnMobileDark.addEventListener("click", makeDark)
+btnMobileLight.addEventListener("click", makeLight)
+
+btnDesktopDark.addEventListener("click", makeDark)
+btnDesktopLight.addEventListener("click", makeLight)
