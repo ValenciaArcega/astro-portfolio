0b0b8c8cc612335ac6c0d9d7390786da63abf6af
ValenciaArcega <valencia.arcega@gmail.com>
2024-04-27 11:58:06 -0600

    deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://valenciaarcega.github.io',
+	// base: 'astro-portfolio',
 	server: {
 		host: '0.0.0.0',
 		open: false,
