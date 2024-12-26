cb020397ef7f9225da1e969e8b3f719c37b59dac
ValenciaArcega <valencia.arcega@gmail.com>
2024-12-25 23:11:47 -0600

    feature:: Update environment types, add react-icons, and modify footer year; refactor appearance functions to TypeScript and new Experience component issues had been solved

---
+++ src/functions/appearance.ts
@@ -0,0 +1,33 @@
+let root = document.documentElement;
+const btnMobileDark = document.querySelector("#btnDarkMobile");
+const btnMobileLight = document.querySelector("#btnLightMobile");
+const btnDesktopDark = document.querySelector("#btnLightDesktop");
+const btnDesktopLight = document.querySelector("#btnDarkDesktop");
+
+const htmlTag = document.querySelector("html");
+
+function makeDark() {
+	htmlTag.classList.add("dark");
+
+	root.id = "rootDarkMode";
+	btnMobileDark.classList.add("hidden");
+	btnMobileLight.classList.remove("hidden");
+	btnDesktopDark.classList.add("hidden");
+	btnDesktopLight.classList.remove("hidden");
+}
+
+function makeLight() {
+	htmlTag.classList.remove("dark");
+
+	root.removeAttribute("id");
+	btnMobileLight.classList.add("hidden");
+	btnMobileDark.classList.remove("hidden");
+	btnDesktopLight.classList.add("hidden");
+	btnDesktopDark.classList.remove("hidden");
+}
+
+btnMobileDark.addEventListener("click", makeDark);
+btnMobileLight.addEventListener("click", makeLight);
+
+btnDesktopDark.addEventListener("click", makeDark);
+btnDesktopLight.addEventListener("click", makeLight);
