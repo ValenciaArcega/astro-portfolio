022b2936ca04524f5d9540a2c6e9838cb051c1b5
ValenciaArcega <valencia.arcega@gmail.com>
2025-04-20 22:51:40 -0600

    03 - Useless CSS custom properies had been removed and new scheme for main color palette has been integrated to replace hard-code main color variants. Also some tailwind migration at Welcome section.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,21 @@ export default {
 	darkMode: "class",
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				p50: "#d9e0fb",
+				p100: "#b3c1f7",
+				p200: "#8ea1f3",
+				p300: "#6882ef",
+				p400: "#5573ed",
+				p500: "#4263eb",
+				p600: "#3b59d4",
+				p700: "#354fbc",
+				p800: "#283b8d",
+				p900: "#1a285e",
+				p950: "#070a17",
+			}
+		},
 	},
 	plugins: [],
 	corePlugins: {
