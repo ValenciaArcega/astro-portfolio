6dda18db6ad69afdf55eaefb86a5090d74e2bf8c
ValenciaArcega <valencia.arcega@gmail.com>
2026-02-04 05:56:22 -0600

    03 - Transitions, semantics and some basic design adjustments.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,14 +8,14 @@ const Footer = function () {
 				target="_blank"
 				href="https://www.linkedin.com/in/luis-angel-valencia-arcega-4827b9239"
 			>
-				<PiLinkedinLogoDuotone className='text-black dark:text-gray-400 hover:!text-blue-400' size={42} />
+				<PiLinkedinLogoDuotone className='text-black dark:text-gray-400 hover:text-blue-400!' size={42} />
 			</a>
 			<a
 				className="decoration-transparent"
 				target="_blank"
 				href="https://github.com/ValenciaArcega"
 			>
-				<PiGithubLogoDuotone className='text-black dark:text-gray-400 hover:!text-blue-400' size={42} />
+				<PiGithubLogoDuotone className='text-black dark:text-gray-400 hover:text-blue-400!' size={42} />
 			</a>
 		</main>
 		<p className="text-base text-black dark:text-white">2025. Valencia Arcega</p>
